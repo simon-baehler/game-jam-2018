01715636e323590dfd9cd090b551407266a711db
Unknown <simon.baehler@heig-vd.ch>
2018-07-25 16:17:28 +0200

    nexus

---
+++ Assets/Scripts/NexusManager.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class NexusManager : MonoBehaviour {
+
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
+    protected int currentTarget;
+    protected GameObject targetGameObject;
+    protected minion minion;
+    protected float timeLeftBeforeShooting;
+    protected bool haveAFocus = false;
+   
+    protected bool canShoot = true;
+
+
+    int Team1TowerIndex;
+    int Team1bulletIndex;
+    int Team2TowerIndex;
+    int Team2bulletIndex;
+
+    const float SHOOTING_INTERVAL = 2.0f;
+     
+    // Use this for initialization
+    void Start()
+    {
+        haveAFocus = false;
+        canShoot = true;
+        timeLeftBeforeShooting = SHOOTING_INTERVAL;
+
+        Team1TowerIndex = LayerMask.NameToLayer("Team1-Nexus");
+        Team1bulletIndex = LayerMask.NameToLayer("Team1-Bullet");
+        Team2TowerIndex = LayerMask.NameToLayer("Team2-Nexus");
+        Team2bulletIndex = LayerMask.NameToLayer("Team2-Bullet");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /*
+     * Descr : 
+     * 
+     */
+    void FireAndRest(Collider2D col)
+    {
+        Vector2 targetPosition = col.gameObject.transform.position;
+        currentTarget = col.gameObject.GetInstanceID();
+        haveAFocus = true;
+        targetGameObject = col.gameObject;
+        if (canShoot)
+        {
+            canShoot = false;
+            timeLeftBeforeShooting = SHOOTING_INTERVAL;
+            this.Fire(targetPosition);
+        }
+    }
+
+    void Fire(Vector2 targetPosition)
+    {
+        // Create the Bullet from the Bullet Prefab
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+
+
+        //spawned bullet team = tower team
+        if (this.gameObject.layer == Team1TowerIndex)
+        {
+            bullet.layer = Team1bulletIndex;
+        }
+        else
+        {
+            bullet.layer = Team2bulletIndex;
+        }
+
+        Vector2 targetVect = new Vector2(targetPosition.x - this.transform.position.x, targetPosition.y - this.transform.position.y);
+        bullet.GetComponent<Rigidbody2D>().velocity = targetVect;
+        Destroy(bullet, SHOOTING_INTERVAL);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (!haveAFocus)
+        {
+
+            Debug.Log("1");
+            if (col.gameObject.tag == "minion")
+            {
+                FireAndRest(col);
+            }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        if (!haveAFocus)
+        {
+            FireAndRest(col);
+        }
+        else
+        {
+            Vector2 targetPosition = targetGameObject.transform.position;
+            currentTarget = col.gameObject.GetInstanceID();
+            haveAFocus = true;
+            if (canShoot)
+            {
+                canShoot = false;
+                timeLeftBeforeShooting = SHOOTING_INTERVAL;
+                this.Fire(targetPosition);
+            }
+        }
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        //set target null
+        if (targetGameObject.GetInstanceID() == collision.GetInstanceID())
+        {
+            currentTarget = 0;
+            targetGameObject = null;
+            haveAFocus = false;
+        }
+
+    }
+}
--- Assets/Scripts/TowerManager.cs
+++ Assets/Scripts/TowerManager.cs
@@ -2,49 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class tower : MonoBehaviour
+public class TowerManager : MonoBehaviour
 {
-    public bool haveAFocus = false;
-    public int currentTarget;
-    public GameObject targetGameObject;
-    public minion minion;
-
-    public float timeLeft = 5.0f;
-    public bool canShoot = true;
 
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
 
+    protected int currentTarget;
+    protected GameObject targetGameObject;
+    protected minion minion;
+
+    protected bool haveAFocus = false;
+    protected float timeLeftBeforeShooting = 5.0f;
+    protected bool canShoot = true;
 
 
+    int Team1TowerIndex;
+    int Team1bulletIndex;
+    int Team2TowerIndex;
+    int Team2bulletIndex;
+
+    const float SHOOTING_INTERVAL = 5.0f;
     // Use this for initialization
     void Start()
     {
-
+        haveAFocus = false;
+        canShoot = true;
+        timeLeftBeforeShooting = SHOOTING_INTERVAL;
     }
     void Update()
     {
-
         if (!canShoot)
         {
-            timeLeft -= Time.deltaTime;
-            if (timeLeft < 0)
+            timeLeftBeforeShooting -= Time.deltaTime;
+            if (timeLeftBeforeShooting < 0)
             {
                 this.canShoot = true;
             }
 
         }
-    }
-
-
-    private void OnDrawGizmos()
-    {
-        if (targetGameObject) {
-            Gizmos.color = Color.red;
-            Vector3 position = targetGameObject.transform.position;
-            Gizmos.DrawSphere(position, 0.1f);
-        }
-
     }
 
     void Fire(Vector2 targetPosition)
@@ -54,10 +50,7 @@ public class tower : MonoBehaviour
             bulletPrefab,
             bulletSpawn.position,
             bulletSpawn.rotation);
-        int direction = 6;
         // Add velocity to the bullet
-
-        Debug.Log("fire");
         int Team1TowerIndex = LayerMask.NameToLayer("Team1-Tower");
         int Team1bulletIndex = LayerMask.NameToLayer("Team1-Bullet");
         int Team2TowerIndex = LayerMask.NameToLayer("Team2-Tower");
@@ -76,6 +69,7 @@ public class tower : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D col)
     {
+
         if (!haveAFocus) {
             //TODO : remplacer name par tag
             if (col.gameObject.tag == "minion")
@@ -87,7 +81,7 @@ public class tower : MonoBehaviour
                 if (canShoot)
                 {
                     canShoot = false;
-                    timeLeft = 5.0f;   
+                    timeLeftBeforeShooting = 5.0f;   
                     this.Fire(targetPosition);
                 }
             }
@@ -108,7 +102,7 @@ public class tower : MonoBehaviour
                 if (canShoot)
                 {
                     canShoot = false;
-                    timeLeft = 5.0f;
+                    timeLeftBeforeShooting = 5.0f;
                     this.Fire(targetPosition);
                 }
             }
@@ -120,7 +114,7 @@ public class tower : MonoBehaviour
             if (canShoot)
             {
                 canShoot = false;
-                timeLeft = 5.0f;
+                timeLeftBeforeShooting = 5.0f;
                 this.Fire(targetPosition);
             }
         }
@@ -128,8 +122,11 @@ public class tower : MonoBehaviour
     private void OnTriggerExit2D(Collider2D collision)
     {
         //set target null
-        currentTarget = 0;
-        targetGameObject = null;
-        haveAFocus = false;
+        if (targetGameObject.GetInstanceID() == collision.GetInstanceID()) {
+            currentTarget = 0;
+            targetGameObject = null;
+            haveAFocus = false;
+        }
+       
     }
 }
