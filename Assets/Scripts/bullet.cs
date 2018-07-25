53760c0871515053d11a77876f538b2e95008682
Paul-Emile <pejoessel@gmail.com>
2018-07-25 22:42:38 +0200

    Welcome Screen

--- Assets/Scripts/bullet.cs
+++ Assets/Scripts/bullet.cs
@@ -11,12 +11,6 @@ public class bullet : MonoBehaviour {
         Debug.Log("Fired");
     }
 
-	// Update is called once per frame
-	void Update ()
-    {
-		
-	}
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "minion") {
