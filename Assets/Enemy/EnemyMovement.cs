94e3bc7e8721d47eaf028e2ebf1f2004648a216f
Paul-Emile <pejoessel@gmail.com>
2018-07-27 15:17:20 +0200

    Enemy Movement as Network

--- Assets/Enemy/EnemyMovement.cs
+++ Assets/Enemy/EnemyMovement.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class EnemyMovement : MonoBehaviour {
+public class EnemyMovement : NetworkBehaviour {
 
     [SerializeField] float moveSpeed = 1f;
 
