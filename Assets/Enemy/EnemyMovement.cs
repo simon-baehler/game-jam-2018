e9e88d37b821f8d9676becd7279d6c756b0fcf24
Unknown <simon.baehler@heig-vd.ch>
2018-07-27 14:39:00 +0200

    tower shooting online

--- Assets/Enemy/EnemyMovement.cs
+++ Assets/Enemy/EnemyMovement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class EnemyMovement : MonoBehaviour {
+public class EnemyMovement : NetworkBehaviour {
 
     [SerializeField] float moveSpeed = 1f;
 
